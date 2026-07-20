cd2156ad890f91c083a7ac9679b4e18cad3c0a84
mithsd <eduardasmiths@gmail.com>
2026-07-19 23:45:05 -0300

    chore: secret key escondida

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -20,13 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-as+z#s8zw2a)h9!^nr%$w2pt=vx$#r%o#k^o9z)_7_*up38v!h'
+from dotenv import load_dotenv
+
+# Load the .env file
+load_dotenv()
+
+# Pull the secret key from environment variables
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -121,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://localhost:5174"]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
